066aa812c7a98183a3ad1e5ddc442b655810ae3e
Robert McDowell <rob@studiocenter.com>
2014-09-27 19:04:08 -0400

    quiz iPhone app

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Robert McDowell on 9/27/14.
+//  Copyright (c) 2014 Studio Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/BNRQuizViewController.m
@@ -0,0 +1,87 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Robert McDowell on 9/27/14.
+//  Copyright (c) 2014 Studio Center. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    // Return the address of the new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender
+{
+    // Step to the next question
+    self.currentQuestionIndex++;
+    // Am I past the last question?
+    if (self.currentQuestionIndex == [self.questions count]) {
+        // Go back to the first question
+        self.currentQuestionIndex = 0;
+    }
+    // Get the string at that index in the questions array
+    NSString *question = self.questions[self.currentQuestionIndex];
+    // Display the string in the question label
+    self.questionLabel.text = question;
+    // Reset the answer label
+    self.answerLabel.text = @"???";
+}
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    // Display it in the answer label
+    self.answerLabel.text = answer;
+}
+
+@end
