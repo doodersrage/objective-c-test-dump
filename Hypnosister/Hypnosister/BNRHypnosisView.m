292ae07721746ebd0bb4ed2f03ca6fc112cff68d
Robert McDowell <rob@studiocenter.com>
2014-09-29 21:54:31 -0400

    BNR CH4

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Robert McDowell on 9/29/14.
+//  Copyright (c) 2014 Studio Center. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
