2b402439e70fbbe13e11146febd209f6abe303cc
rob <rob@studiocenter.com>
2014-10-03 16:54:58 -0400

    BNR ch8

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by rob on 10/3/14.
+//  Copyright (c) 2014 Studio Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/Homepwner/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by rob on 10/3/14.
+//  Copyright (c) 2014 Studio Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
